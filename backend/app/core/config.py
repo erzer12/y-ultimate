a5af0a0ab0811c561b1eca5bb682e5339e800d58
Harshil P <162300039+erzer12@users.noreply.github.com>
2025-11-01 19:18:07 +0530

    v3.6

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -20,6 +20,7 @@ class Settings(BaseSettings):
     class Config:
         env_file = ".env"
         env_file_encoding = "utf-8"
+        extra = "ignore"
 
 # Create a single instance of the settings to be imported
 # by other parts of the application.
---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,4 @@
+from . import child_profile
+from . import token
+from . import tournament
+from . import user
